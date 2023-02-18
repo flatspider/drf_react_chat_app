d19b8b289fcb7fea5f2c649066efc379fe47dea6
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-02-18 15:38:28 -0500

    Added outlines of frontend

---
+++ frontend/static/src/components/ChatItem.js
@@ -0,0 +1,1 @@
+// A chat item will contain the text and the name of the user who sent the text item
---
+++ frontend/static/src/components/MessagesForm.js
@@ -0,0 +1,2 @@
+// This form will contain as many chat items are within the selected channel.
+// It will also contain the text input and submit button to send a message to the chat.
