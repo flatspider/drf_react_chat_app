f3fcd642c74f1e458f9bc509826e3f0ea02adb30
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-03-03 14:15:45 -0500

    Added new login/register app image.

--- frontend/static/src/components/LoginForm.js
+++ frontend/static/src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Cookies from "js-cookie";
+import MyImage from "./assets/friend-app.webp";
 
 function LoginForm(props, current_user) {
   // Can create a css object up here:
@@ -61,11 +62,7 @@ function LoginForm(props, current_user) {
       <div className="container-fluid h-custom mt-5">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-              className="img-fluid"
-              alt="Sample image"
-            />
+            <img src={MyImage} className="img-fluid" alt="Sample image" />
           </div>
           <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
             <form onSubmit={handleSubmit}>
